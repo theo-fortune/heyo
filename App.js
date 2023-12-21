4355e81707723b30af08712fbf904ba1d124e69e
Theo Fortune <theofortunefunction@gmail.com>
2023-12-21 13:45:17 +0100

    Status Bar

--- App.js
+++ App.js
@@ -1,37 +1,32 @@
-import { View, Button, Pressable, Text, Modal, ScrollView } from "react-native";
+import { View, StatusBar, Button } from "react-native";
 import { useState } from "react";
 const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [isStatusBarVisible, setIsStatusBarVisible] = useState(true);
+  const toggleStatusBar = () => {
+    setIsStatusBarVisible(!isStatusBarVisible);
+  };
+
   return (
-    <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <ScrollView>
-        <Button
-          title="Press"
-          onPress={() => {
-            setIsModalVisible(true);
-          }}
-          color="midnightblue"
-        />
-        <Modal
-          visible={isModalVisible}
-          onRequestClose={() => setIsModalVisible(false)}
-          animationType="slide"
-          presentationStyle="pageSheet"
-        >
-          <View style={{ flex: 1, backgroundColor: "lightblue", padding: 60 }}>
-            <Text>Modal Content</Text>
-            <Button
-              color="midnightblue"
-              title="Close"
-              onPress={() => {
-                setIsModalVisible(false);
-              }}
-            />
-          </View>
-        </Modal>
-      </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "plum",
+        padding: 60,
+      }}
+    >
+      <StatusBar
+        backgroundColor="gold"
+        barStyle="dark-content"
+        hidden={!isStatusBarVisible}
+      />
+      <Button
+        title={isStatusBarVisible ? "Press Me" : "UnPress Me"}
+        onPress={toggleStatusBar}
+      />
     </View>
   );
 }
