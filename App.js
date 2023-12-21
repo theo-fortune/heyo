605186caa5a19e38a9923dbdc844bfe7bbf509fc
Theo Fortune <theofortunefunction@gmail.com>
2023-12-21 12:13:33 +0100

    Image

--- App.js
+++ App.js
@@ -1,11 +1,20 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, ImageBackground } from "react-native";
+const logoImg = require('./assets/adaptive-icon.png')
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      <Text>
-        <Text style={{color: "wheat"}}>Hello</Text>World
+      {/* <Text>
+        <Text style={{ color: "wheat" }}>Hello</Text>World
       </Text>
+      <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      <Image
+        source={{ uri: "https://picsum.photos/300"}}
+        style={{ width: 300, height: 300 }}
+      /> */}
+      <ImageBackground source={logoImg} style={{flex: 1}}>
+        <Text>Image text</Text>
+      </ImageBackground>
     </View>
   );
 }
