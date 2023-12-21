1ed046a0f3edf3188efa0bb67d82d2983f97210c
Theo Fortune <theofortunefunction@gmail.com>
2023-12-21 12:20:39 +0100

    Scroll View

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { View, Text, Image, ImageBackground } from "react-native";
-const logoImg = require('./assets/adaptive-icon.png')
+import { View, Text, Image, ImageBackground, ScrollView } from "react-native";
+const logoImg = require("./assets/adaptive-icon.png");
 
 export default function App() {
   return (
     <View style={{ flex: 1, backgroundColor: "plum", padding: 60 }}>
-      {/* <Text>
-        <Text style={{ color: "wheat" }}>Hello</Text>World
-      </Text>
-      <Image source={logoImg} style={{ width: 300, height: 300 }} />
-      <Image
-        source={{ uri: "https://picsum.photos/300"}}
-        style={{ width: 300, height: 300 }}
-      /> */}
-      <ImageBackground source={logoImg} style={{flex: 1}}>
-        <Text>Image text</Text>
-      </ImageBackground>
+      <ScrollView>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+        <Text>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio
+          aut nihil, reiciendis dicta ullam, debitis a minima mollitia maxime
+          magni praesentium quo, est voluptatibus porro impedit nesciunt?
+          Eveniet voluptatibus pariatur ad quo eligendi quaerat tempore
+          explicabo possimus commodi quod maiores, minus dignissimos culpa
+          architecto praesentium. Nisi ea ab, ullam excepturi ex ipsam provident
+          obcaecati corporis numquam, eum repellat maxime ipsum quod, recusandae
+          cum voluptas earum nam sit! Quam rem deleniti ratione voluptatibus
+          adipisci. Eum unde iusto in! Hic sequi quidem doloribus laudantium
+          tempore? Officia minus unde architecto possimus fugit veniam vel rerum
+          inventore enim, maiores asperiores quisquam. Tempora, accusantium
+          porro.
+        </Text>
+        <Image source={logoImg} style={{ width: 300, height: 300 }} />
+      </ScrollView>
     </View>
   );
 }
