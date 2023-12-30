1fb6c833191f7f750f400087deccab40adf7aa75
Theo Fortune <theofortunefunction@gmail.com>
2023-12-30 03:01:57 +0100

    Platform Specific code

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
-import { View, StyleSheet, Text, SafeAreaView } from "react-native";
+import { View, StyleSheet, Text, SafeAreaView, Platform } from "react-native";
+import CustomButton from "./components/CustomButton/CustomButton";
 
 export default function App() {
   return (
@@ -6,6 +7,7 @@ export default function App() {
       <View style={styles.container}>
         <View style={styles.box}>
           <Text style={styles.text}>Welcome!</Text>
+          <CustomButton title="Press me" onPress={()=>alert("Pressed!")} />
         </View>
       </View>
     </SafeAreaView>
@@ -15,16 +17,28 @@ export default function App() {
 const styles = StyleSheet.create({
   safeContainer: {
     flex: 1,
-    backgroundColor: "plum"
+    backgroundColor: "plum",
   },
   container: {
     flex: 1,
     backgroundColor: "plum",
+    paddingTop: Platform.OS === "android" ? 25 : 0,
   },
   box: {
     padding: 20,
   },
   text: {
+    ...Platform.select({
+      ios: {
+        color: "purple",
+        fontSize: 24,
+        fontStyle: "italic"
+      },
+      android: {
+        color: "blue",
+        fontSize: 30
+      }
+    }),
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
