83d5de2e6c46090c39e5ac09f66b5b12e1a2f764
Theo Fortune <theofortunefunction@gmail.com>
2023-12-30 02:14:48 +0100

    dimensions api

--- App.js
+++ App.js
@@ -1,32 +1,34 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Text, Dimensions } from "react-native";
 import Box from "./components/Box";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "#8e9b00", flexShrink: 1 }}>
-        Box 1 Shrink
-      </Box>
-      <Box style={{ backgroundColor: "#b65d1f", flexShrink: 1 }}>
-        Box 2 Shrink
-      </Box>
-      {/* <Box style={{ backgroundColor: "#1c4c56" }}>Box 3</Box>
-      <Box style={{ backgroundColor: "#ab9156" }}>Box 4</Box>
-      <Box style={{ backgroundColor: "#6b0803" }}>Box 5</Box>
-      <Box style={{ backgroundColor: "#1c4c56" }}>Box 6</Box>
-      <Box style={{ backgroundColor: "#b95f21" }}>Box 7</Box> */}
+      <View style={styles.box}>
+        <Text style={styles.text}>Welcome!</Text>
+      </View>
     </View>
   );
 }
 
+const windowWidth = Dimensions.get("window").width;
+const windowHeight = Dimensions.get("window").height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    width: 300,
-    flexDirection: "row",
-    alignItems: "flex-start",
-    marginTop: 64,
-    borderWidth: 6,
-    borderColor: "red",
+    backgroundColor: "plum",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  box: {
+    width: windowWidth > 500 ? "70%" : "90%",
+    height: windowHeight > 600 ? "60%" : "90%",
+    backgroundColor: "lightblue",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontSize: windowWidth > 500 ? 50 : 24,
+  }
 });
--- components/Box.js
+++ components/Box.js
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from "react-native";
 
-export default function Box({children, style}) {
+export default function Box({ children, style }) {
   return (
     <View style={[styles.box, style]}>
       <Text style={styles.text}>{children}</Text>
@@ -12,12 +12,12 @@ const styles = StyleSheet.create({
   box: {
     backgroundColor: "#fff",
     padding: 20,
+    flexGrow: 1,
   },
   text: {
     fontSize: 24,
     fontWeight: "bold",
     textAlign: "center",
     color: "white",
-    
-  }
-})
+  },
+});
